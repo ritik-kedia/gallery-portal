0425b4867e817865a5f4574c570c3566b68be131
ritik-kedia <ritikkedia039@gmail.com>
2026-03-25 16:42:04 +0530

    fixes

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const path = require("path");
 
 
-const uploadDir = path.join(__dirname, "..", "uploads");
+const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
+  fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
